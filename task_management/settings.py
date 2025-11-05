218e2cc89720667475071cedfd64311e809f6eb8
Moumita Banik <moumita.banik@webguru-development.com>
2025-11-05 16:32:14 +0530

    Update settings.py

--- task_management/settings.py
+++ task_management/settings.py
@@ -143,8 +143,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'jsdev03012022@gmail.com'
-EMAIL_HOST_PASSWORD = 'jdyqirniiajnbogb'
+
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 CELERY_BROKER_URL = "redis://localhost:6379/0"
